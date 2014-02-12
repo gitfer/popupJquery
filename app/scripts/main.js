85dcbc5b9f38ee23badb9891724d64aae468069d
Federico Rota <federico.rota01@gmail.com>
2014-02-12 18:44:39 +0100

    First stub

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,50 @@
-$(function () {
+$(function() {
 	var $primo = $('#primo');
 	var $secondo = $('#secondo');
-	$primo.on('click', function () {
+	var $dialog = $('#dialog');
+	$primo.on('click', function() {
 		alert('primo');
 	});
-	$secondo.on('click', function () {
-		alert('secondo');
+	$secondo.on('click', function() {
+		$dialog.dialog({
+			dialogClass: 'no-close',
+			draggable: true,
+			resizable: false,
+			beforeClose: function(event, ui) {
+				$dialog.text('Prova');
+			},
+			buttons: [{
+					text: "Ok",
+					class: "ui-button-spl1",
+					click: function() {
+						// chiamata ajax
+					}
+				}, {
+					text: "Chiudi",
+					priority: 'secondary',
+					class: "ui-button-spl2",
+					click: function() {
+						$dialog.text('ciao');
+						$(this).dialog({
+							buttons: [{
+								text: "Ok",
+								class: "ui-button-spl1",
+								click: function() {
+									// chiamata ajax
+								}
+							}, {
+								text: "Chiudi",
+								priority: 'secondary',
+								class: "ui-button-spl2",
+								click: function() {
+									$(this).dialog("close");
+								}
+							}]
+						});
+					}
+				}
+
+			]
+		});
 	});
 });
